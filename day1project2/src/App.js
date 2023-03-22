c20210c3ec0b40ca55ffb301404888239d5bc7bd
727721euec041 <727721euec041@skcet.ac.in>
2023-03-22 21:25:54 +0530

    first commit

---
+++ day1project2/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        welcome
+      </h1>
+      <button onClick={window[button]}>
+HOLA! CLICK ME
+      </button>
+    </div>
+  );
+}
+
+export default App;
